01039eb94747ab0e9d45cb978c06f34a31037bb9
truongbo17 <truongisgay5@gmail.com>
2022-03-12 08:04:52 +0700

    add backpack/filemanager

--- config/app.php
+++ config/app.php
@@ -161,11 +161,10 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
-
+        Barryvdh\Elfinder\ElfinderServiceProvider::class,
         /*
          * Application Service Providers...
          */
